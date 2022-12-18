2c3c88b79405f4a48a74f7c87f4ca0441fb3921b
Anastasia Yarceva <jartzeva.nastya@yandex.ru>
2022-12-18 18:29:22 +0300

    Изменение кредо

--- js.js
+++ js.js
@@ -3,10 +3,6 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('Все в жизни возможно, главное, насколько сильно ты этого хочешь :) P.S. Если у меня получилось стать QA, все точно возможно!')
+	alert('Ты классный! Никогда не забывай про это :)')
 })
 
-
-button.addEventListener('click', function openImage(gif){
-	image='gif1.gif'
-})
